712866dac6b198322d486a2156b9b69cb51dd1c0
yo-yan <yooyan0512@gmail.com>
2021-02-06 21:51:56 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+//pushの練習
+const App: React.FC<{}> = () => {
+
+  const [count, setCount] = useState<number>(0);
+
+  const handleUp = () => {
+    setCount(count + 1)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>App.tsx</h1>
+      <h1>{count}</h1>< br />
+      <button onClick={handleUp}>count up</button>
+
+
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { UserType } from '../types/UserType';
+const Login: React.FC<UserType> = () => {
+    return (
+        <div>
+            <h1>Login.tsx</h1>
+        </div>
+    )
+}
+
+export default Login
